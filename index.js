31f1537de52fdd960145f3f204b2e56c0cde0a5c
Eivind Madsen Pham <eivindmpham@gmail.com>
2019-10-03 11:52:15 +0200

    Fixed button with link

--- index.js
+++ index.js
@@ -9,3 +9,14 @@ function typeWriter() {
     setTimeout(typeWriter, speed);
   }
 }
+
+document.querySelector("#dyrevern").addEventListener('click', (e) => {
+  gtag('event', 'click', {
+      'event_category': 'outbound',
+      'event_label': 'dyrevern',
+      'transport_type': 'beacon',
+      'event_callback': function () {
+          window.location = 'http://www.dyrevern.no/stott_oss';
+      }
+  });
+});
