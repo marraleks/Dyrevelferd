e7457f40f6f88e5c92321692c41cd56cd8c71573
Marius Aleksander <marraleks@hotmail.com>
2019-10-07 09:23:18 +0200

    La til cursor

--- index.js
+++ index.js
@@ -20,3 +20,17 @@ document.querySelector("#dyrevern").addEventListener('click', (e) => {
       }
   });
 });
+
+const cursor = document.querySelector('.cursor');
+
+document.addEventListener('mousemove', e => {
+    cursor.setAttribute("style", "top: "+(e.pageY - 10)+"px; left: "+(e.pageX - 10)+"px;")
+})
+
+document.addEventListener('click', () => {
+    cursor.classList.add("expand");
+
+    setTimeout(() => {
+        cursor.classList.remove("expand");
+    }, 500)
+})
