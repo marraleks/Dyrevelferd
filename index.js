256f13dfac245759cbbc8ce7b7777e6105560747
Marius Aleksander <marraleks@hotmail.com>
2019-10-09 17:13:45 +0200

    dyrevern script

--- index.js
+++ index.js
@@ -10,6 +10,17 @@ function typeWriter() {
   }
 }
 
+document.querySelector("#dyrevern").addEventListener('click', (e) => {
+  gtag('event', 'click', {
+      'event_category': 'outbound',
+      'event_label': 'dyrevern',
+      'transport_type': 'beacon',
+      'event_callback': function () {
+          window.location = 'http://www.dyrevern.no/stott_oss';
+      }
+  });
+});
+
 const cursor = document.querySelector('.cursor');
 
 document.addEventListener('mousemove', e => {
